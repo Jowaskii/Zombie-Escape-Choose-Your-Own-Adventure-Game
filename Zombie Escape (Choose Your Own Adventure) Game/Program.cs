eda3a144706b65c30a116f7afdbfc642dc985b72
Jowaskii <jerome.jowaskicharles@gmail.com>
2026-05-19 22:49:37 -0400

    Add project files.

---
+++ Zombie Escape (Choose Your Own Adventure) Game/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintTitleScreen()
+{
+    Console.WriteLine("" +
+        "*******************************************************************************\r\n" +
+        "                          .-\"L\"-.  \r\n" +
+        "                            \r\n" +
+        "          .--\"\"L\"\"---___    J  |  |\r\n" +
+        "          J    J        \"\"L  L J  J              J----\"\"\"J\"\"---__\r\n" +
+        "           |    |         J  J  L  L_--\"\"|\"--L   |       F      J\r\n" +
+        "            L    L__ ___   L  L |  J     |   J   F      J       F\r\n" +
+        "         \\\"\"\"\"\"\"\"   |   \"\"\"\"\"-----, L    |   J  J       F      |\r\n" +
+        "          \\          L            L L    |   |  |      J      J      _.---\"\"\"\r\n" +
+        "           \\         J            | |    |   |  F      F      F     /\r\n" +
+        "            \\         |           J |    |   | J      J      |     /\r\n" +
+        "             \\         L      ___---++...;.++'--__    F     J     /\r\n" +
+        "              \\        J      L\"\"----____ __---\"\"J__--=----_E_   /\r\n" +
+        "               \\        |     J          |       |--__   __--\"/ /\r\n" +
+        "          __    \\        L     L         |       F    \"\"F    / /\r\n" +
+        "            \"\"\"--__      J     J         |      J      |    / /\r\n" +
+        "                   \"\";    |     L        |      |     J    / /\r\n" +
+        "              __--\"\"\" \\    L    J        |      F     F   / /\r\n" +
+        "        __--\"\"         \\   J     L       |     J     |   / /\r\n" +
+        "    --\"\"                \\   |    J       |     |    J   / /\r\n" +
+        "                         \\   L    L      |     F    F  / /\r\n" +
+        "                          \\  J    J      |    J    |  / /\r\n" +
+        "                           \\  |    L     |    |   J  / /\r\n" +
+        "                            \\  L   J     |    F   F / /\r\n" +
+        "                             \\ J    L    |   J   | / /\r\n" +
+        "                              \\ |  _J    |   |\"-J+' /\r\n" +
+        "                               \\-L\"  L   |   F     /\r\n" +
+        "                                \\    J   |  J     /\r\n" +
+        "        '-.                      \\    \"-.|+'     /\r\n" +
+        "           '-.                    \\             /                   .\r\n" +
+        "              '-.                  \\           /                 .-'\r\n" +
+        "                 '-.             .-'            \"-.           .-'\r\n" +
+        "                hs  '-.       .-'        .         \"-.     .-'\r\n" +
+        "                       '-. .-'         .' '.          '-.-'\r\n" +
+        "                          '          .'     '.\r\n" +
+        "    \r\n" +
+        "    *******************************************************************************");
+    Console.WriteLine("Welcome to Zombie Escape!");
+    Console.WriteLine("In this game, you will make choices to survive a zombie apocalypse.");
+    Console.WriteLine("Good luck!");
+    Console.WriteLine("Press Enter to start...");
+    Console.ReadLine();
+}
+PrintTitleScreen();
+Console.Clear();
+//Console.WriteLine("You wake up in a small, dimly lit room. You can hear the groans of zombies outside. What do you do?");
+
+//Player wakes up and sees zombies outside their apartment window
+Console.WriteLine("You wake up and hear screaming outside your apartment window!\nYou look outside in horror as chaos unfolds in front of you, " +
+    "crashed cars screaming civilians being attacked and torn to pieces.\n" +
+    "The once beautiful Point Guard City is being over-runed by zombies!! YOU MUST ESCAPE");
+Console.WriteLine("Press Enter to continue...");
+Console.ReadLine();
+
+//Openning choice
+
